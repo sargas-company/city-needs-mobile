72053135613b55f68b69bf95050a27cb5d77ddbc
Dmytro Sarafaniuk <sarafaniuk.dima@gmail.com>
2025-12-05 16:55:12 +0200

    first commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require('eslint/config')
+const expoConfig = require('eslint-config-expo/flat')
+const prettierPlugin = require('eslint-plugin-prettier')
+const prettierConfig = require('eslint-config-prettier')
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
-]);
+    ...expoConfig,
+    {
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        rules: {
+            'prettier/prettier': 'warn',
+        },
+    },
+    prettierConfig,
+    {
+        ignores: ['dist/', 'build/', '.expo/'],
+    },
+])
