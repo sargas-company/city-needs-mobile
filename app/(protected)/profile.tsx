5e6f6d908127fa5a55ec6e9fe16036f6fb2005f5
Dmytro Sarafaniuk <sarafaniuk.dima@gmail.com>
2025-12-08 23:55:59 +0200

    imports fix

--- app/(protected)/profile.tsx
+++ app/(protected)/profile.tsx
@@ -1,10 +1,10 @@
-import { SafeAreaView, Text } from 'react-native'
+import { Text, View } from 'react-native'
 
 const Profile = () => {
     return (
-        <SafeAreaView className="flex-1 items-center justify-center bg-white">
+        <View className="flex-1 items-center justify-center bg-white">
             <Text className="text-xl font-bold text-black">Profile</Text>
-        </SafeAreaView>
+        </View>
     )
 }
 
